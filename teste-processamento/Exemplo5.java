6ac5a860e5f01a66ba933ea23625cb3cadcba156
Cargnieli2016 <cargnieli2016@gmail.com>
2024-06-18 15:21:57 -0300

    Iniciando cursdo de Java com Nelio Alves

---
+++ teste-processamento/Exemplo5.java
@@ -0,0 +1,14 @@
+
+public class Exemplo5 {
+	public static void main(String[] args) {
+		int a, b;
+		double resultado;
+		
+		a = 5;
+		b = 2;
+		
+		resultado = (double) a / b; // casting
+		System.out.println(resultado);
+	}
+
+}
